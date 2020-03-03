32828f8fc70ac39abf8ef60aac4514d19db487aa
Parvej <parvejmallick@yahoo.com>
2020-03-03 13:31:54 +0000

    Fix

--- app.js
+++ app.js
@@ -2,18 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-var Web3 = require('./node_modules/web3/index.js');
+var Web3 = require('web3');
 var web3 = new Web3();
 web3.setProvider(new web3.providers.HttpProvider('http://127.0.0.1:8545'));
 
 
 app.get('/', (req, res) => {
-	if(!web3.isConnected()){
-    		console.log("not connected");
-	}else{
-    		console.log("connected");
-	}
-	res.send('Hello World!')
+	web3.eth.net.isListening().then(() => res.send('is connected'))
+   	.catch(e => res.send('Wow. Something went wrong'));
+	//res.send('Hello World!')
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
